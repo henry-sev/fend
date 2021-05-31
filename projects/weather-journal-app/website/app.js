ec3b7a9c3ae7e7eab8838715c675998532317466
henry-sev <q2466351926@gmail.com>
2021-05-31 15:30:47 +0800

    Get weather by weather API

--- projects/weather-journal-app/website/app.js
+++ projects/weather-journal-app/website/app.js
@@ -1,4 +1,22 @@
 /* Global Variables */
+const baseURL = "https://api.openweathermap.org/data/2.5/weather?q=";
+const apiKey = "&appid=b4f9bc96486cca988708b41a0aae131b";
+// const weatherAPI = baseURL + zipcode + apiKey;
+
+document.querySelector('#generate').addEventListener('click', getWeather);
+
+async function getWeather() {
+  const zipcode = document.querySelector('#zip').value;
+  const res = await fetch(baseURL + zipcode + apiKey);
+
+  try {
+    const weather = await res.json();
+    console.log(weather);
+  } catch(error) {
+    console.log("error", error);
+  }
+}
+
 
 // Create a new date instance dynamically with JS
 let d = new Date();
