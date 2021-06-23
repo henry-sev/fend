a213c46b416ccee7d76d6851e45e8412ba218978
henry-sev <q2466351926@gmail.com>
2021-06-23 15:17:05 +0800

    Minimize Css

--- projects/evaluate-news-nlp/webpack.prod.js
+++ projects/evaluate-news-nlp/webpack.prod.js
@@ -1,10 +1,16 @@
 const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const TerserPlugin = require('terser-webpack-plugin')
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 
 module.exports = {
     entry: './src/client/index.js',
     mode: 'production',
+    optimization: {
+        minimizer: [new TerserPlugin({}), new OptimizeCssAssetsPlugin({})]
+    },
     module: {
         rules: [
             {
@@ -15,7 +21,7 @@ module.exports = {
 
             {
                 test: /\.css$/,
-                loader: ['style-loader', 'css-loader']
+                loader: [MiniCssExtractPlugin.loader, 'css-loader']
             }
         ]
     },
@@ -23,6 +29,7 @@ module.exports = {
         new HtmlWebPackPlugin({
             template: "./src/client/views/index.html",
             filename: "./index.html",
-        })
+        }),
+        new MiniCssExtractPlugin(),
     ]
 }
